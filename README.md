e50f79c477b8e9fd49865dd9f6e1a440aaa837bb
rjo6615 <robertjobernier@gmail.com>
2022-09-29 17:30:17 -0400

    Added arrays and function to display random password

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Module-3-Challenge
+
+https://www.tutorialspoint.com/split-number-into-4-random-numbers-in-javascript
--- script.js
+++ script.js
@@ -1,14 +1,66 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lettersUpper = letters.map(element => {
+  return element.toUpperCase();
+});
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+// get backslash to work
+
+var passwordLength = 8;
+
+var lettersChoice = true;
+var lettersUpperChoice = true;
+var numbersChoice = true;
+var specialChoice =  true;
+
+console.log(special);
+
+// functions
+
+function generateCharacter(useLetters, useUppers, useNumbers, useSpecial) {
+  var set = []
+  if (useLetters) set = set.concat(letters)
+  if (useUppers) set = set.concat(lettersUpper)
+  if (useNumbers) set = set.concat(numbers)
+  if (useSpecial) set = set.concat(special)
+  return set[Math.floor(Math.random() * set.length)];
+}
+
+// execution
+var password = ""
+var length = 8
+
+for(var i = 0; i < length; i++){
+  password += generateCharacter(false, true, true, true)
+}
+
+console.log(password);
+
+
+
+
+
+// testing
+// console.log(generatePassword());
+
+
+
+
 
 // Write password to the #password input
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
+
+
 generateBtn.addEventListener("click", writePassword);
